3302920a79d6130e62d51dce278c7abd8bd998cf
Hiwa Shaloudegi <hiwa.shaloudegi@gmail.com>
2022-01-08 19:06:57 +0330

    added logout feature

--- src/users/views.py
+++ src/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate 
 
@@ -12,9 +12,10 @@ def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f"{username} Account Created")
+            user = form.save()
+            request.session['user'] = user.id
+            # username = form.cleaned_data.get('username')
+            messages.success(request, f"{user.username} Account Created")
             return redirect("index")
     else:
         form = UserCreationForm()
@@ -27,25 +28,39 @@ def register(request):
 
 
 def login(request):
-    print(f" USER OBJECTS ALL: {User.objects.all()}")
     if request.method == "POST":
         username = request.POST.get('username') 
         password = request.POST.get('password')
 
-        user = authenticate(username=username, password=password)
-        # user = User.objects.all().filter(username=username)       # WTF!: empty list?? # , password__exact=password
+        # form = AuthenticationForm(data=request.POST)
+        # if form.is_valid():
+        #     request.session['user'] = form.get_user().id
+        #     messages.info(request, f"You are loged in as {username}")  # {form.get_user().username}
+        #     return redirect('index') 
+        # else:
+        #     messages.warning(request, "Invalid username or password! please try again")
 
-        if user is not None:    # user.exists()
-            request.session['user'] = username
+        user = authenticate(username=username, password=password)
+        # user = User.objects.all().filter(username=username)           # WTF!: empty list?? # , password__exact=password
+
+        if user is not None:       # user.exists()
+            request.session['user'] = user.id                           # request.session['user'] = username
             messages.info(request, f"You are loged in as {username}")
             return redirect('index') # return redirect('profile')
         else:
             messages.warning(request, "Invalid username or password! please try again")
+            return render(request, 'users/login.html')
 
     return render(request, 'users/login.html')
 
 
 
-
 def logout(request):
-    pass
+    del request.session['user']
+    messages.info(request, "Logged out successfully!")
+    print("Logged out successfully!")
+
+    return redirect('index')
+
+    
+
